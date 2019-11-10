d9c449752e748a5f1e4814f08ef11cf397870359
Gál Péter <pepyta118@gmail.com>
2019-11-10 11:30:54 +0100

    Switch back to the original api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const followers = require('instagram-followers');
+let Instagram = require('instagram-nodejs-without-api');
+Instagram = new Instagram()
 const express = require('express');
 const app = express();
 
@@ -14,9 +15,19 @@ app.get("/", (request, response) => {
 app.listen(8080)
 
 setInterval(() => {
-    followers('tftcentral').then(no => {
-        console.log(no)
-    })
+    Instagram.getCsrfToken().then((csrf) => {
+        Instagram.csrfToken = csrf;
+    }).then(() => {
+        return Instagram.auth(process.env.INSTAGRAM_USERNAME, process.env.INSTAGRAM_PASSWORD).then(sessionId => {
+            Instagram.sessionId = sessionId
+
+            return Instagram.getUserDataByUsername('tftcentral').then((t) => {
+                client.channels.get("642886967100440591").setName(`Instagram followers: ${t['graphql']['user']['edge_followed_by']['count']}`)
+                console.log(t['graphql']['user']['edge_followed_by']['count'])
+            })
+
+        })
+    }).catch(console.error);
     client.channels.get("642884636539879443").setName(`Discord users: ${client.channels.get("642884636539879443").guild.memberCount}`)
 }, 10000)
 
