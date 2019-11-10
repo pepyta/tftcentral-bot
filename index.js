8db67df11679a498b9c055e33c9fd442386e3969
Gál Péter <pepyta118@gmail.com>
2019-11-10 11:48:44 +0100

    Convert to "k" form ig followers

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ setInterval(() => {
             Instagram.sessionId = sessionId
 
             return Instagram.getUserDataByUsername('tftcentral').then((t) => {
-                client.channels.get("642886967100440591").setName(`Instagram followers: ${t['graphql']['user']['edge_followed_by']['count']}`)
-                console.log(t['graphql']['user']['edge_followed_by']['count'])
+                var followers = t['graphql']['user']['edge_followed_by']['count']
+                client.channels.get("642886967100440591").setName(`Instagram followers: ${(followers/1000+"").split(".")[0]+((followers/1000+"").split(".")[1].substring(0,1) > 0 ? "."+(followers/1000+"").split(".")[1].substring(0,1) : "")}k`)
+                console.log(`${(followers/1000+"").split(".")[0]+((followers/1000+"").split(".")[1].substring(0,1) > 0 ? "."+(followers/1000+"").split(".")[1].substring(0,1) : "")}k`)
             })
 
         })
