077df6b61a55e9d155965f2f2c360d68ea6a51b1
Gál Péter <pepyta118@gmail.com>
2020-03-20 00:22:24 +0100

    Add set 3 data

---
+++ modules/champions.js
@@ -0,0 +1,63 @@
+const champions = require('../dataset/champions.json')
+const traits = require('../dataset/traits.json')
+const Fuse = require('fuse.js')
+
+var tmp = {}
+traits.forEach(function(elem){
+    tmp[elem.name] = elem
+})
+
+module.exports = {
+    generateEmbed: function (championName) {
+        var data = searchChampionData(championName)
+
+        var fields = []
+
+        data['tr'].forEach(function(trait){
+            fields.push({
+                name: trait['name'],
+                value: `${trait['innate'] ? `*Innate:* ${trait['innate']}\n*Description*: ` : ""}${trait['description']}`
+            })
+        })
+
+        return {
+            "embed": {
+              "color": 5573598,
+              "footer": {
+                "icon_url": "https://www.escharts.com/storage/app/uploads/public/5d2/355/0d1/5d23550d15183441256444.png",
+                "text": "Set 3"
+              },
+              "thumbnail": {
+                "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+              },
+              "author": {
+                "name": data['name']
+              },
+              "fields": fields
+            }
+          }
+    }
+}
+
+function searchChampionData(champion) {
+    let options = {
+        shouldSort: true,
+        threshold: 0.6,
+        location: 0,
+        distance: 1,
+        minMatchCharLength: 1,
+        keys: [
+            "name",
+            "championId"
+        ]
+    }
+
+    let fuse = new Fuse(champions, options)
+    let result = fuse.search(champion)
+
+    console.log(result[0])
+    result[0]['item']['traits'].forEach(function(trait){
+        result[0]['item']['tr'] = tmp[trait]
+    })
+    return result[0]['item']
+}
